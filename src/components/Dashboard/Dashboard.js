fdb1f744401eb1b61b5135713830208796ff7114
Lola Simpson <lolalsimp@gmail.com>
2018-07-23 21:20:55 -0500

    child is saving with parentId in firebase

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 import './Dashboard.css';
 
+
 class Dashboard extends React.Component {
-  render () {
-    return (
-      <div className = "Dashboard">
-        <h2 className="title">Dashboard</h2>
-      </div>
-    );
+    render() {
+      return (
+       <div>
+         <button><Link to="/new/child">Go To New</Link></button>
+       </div>
+      );
+    }
   }
-}
 
 export default Dashboard;
