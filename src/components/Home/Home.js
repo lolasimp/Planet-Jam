4bd877f0f303919a5d6d349f0bc3d069342cf462
Lola Simpson <lolalsimp@gmail.com>
2018-07-31 19:15:18 -0500

    added new avatars

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import './Home.css';
+import radioAstro from '../../assets/images/radio-astro.png';
+// import saucer from '../../assets/images/saucer.png';
 
 class Home extends React.Component {
   render () {
     return (
       <div className = "Home">
         <h2 className="title">Planet Jam</h2>
+        <img src={radioAstro} alt=""/>
       </div>
     );
   }
