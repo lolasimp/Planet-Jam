aa2670cae2ccb391e925242bf62ac43af9f7175e
Lola Simpson <lolalsimp@gmail.com>
2018-08-17 22:06:21 -0500

    added modal

--- src/components/PlanetKeys/PlanetKeys.js
+++ src/components/PlanetKeys/PlanetKeys.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import {Modal, Button} from 'react-bootstrap';
+
 import save from '../../assets/images/save.png';
 
 import './PlanetKeys.css';
 
 class PlanetKeys extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+
+    this.state = {
+      show: false
+    };
+  }
+
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
+  }
+
   static propTypes = {
     details: PropTypes.object.isRequired,
   }
@@ -41,15 +62,32 @@ class PlanetKeys extends React.Component {
       <li>
       <div className="planet-container col-xs-4">
         <h2 className="planetName">{details.name}</h2>
-        <button type="button" className="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-sm">Planet Info</button>
+      <div>
+        <Button bsStyle="primary" bsSize="large" onClick={this.handleShow}>
+          Launch demo modal
+        </Button>
 
-<div className="modal fade bs-example-modal-sm" tabIndex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
-  <div className="modal-dialog modal-sm" role="document">
-    <div className="modal-content">
-    <h2>Hello</h2>
-    </div>
-  </div>
-</div>
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Fun Fact</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h4>MARS</h4>
+            <p>
+              Mars is the fourth planet from the Sun and last of the terrestrial planets.
+              Mars is sometimes called the Red Planet because of the brownish-red color of its surface.
+              Mars is the second smallest planet in the solar system behind Mercury.
+              The tallest mountain known in the solar system is on Mars. 
+              Olympus Mons is a 21 km high and 600 km diameter shield volcano that was formed billions of years ago. 
+              Mars experiences huge dust storms – the largest in our solar system. 
+              This is due to the elliptical shape of the planet’s orbit path around the Sun.
+            </p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.handleClose}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
         <div className=" img__wrap">
        <img className="planet-pic" src={imagePath} alt={details.name} onClick={this.playSoundEvent} id={details.id}/>
        </div>
